a46167073596022250c484def0f75ac8b9984a36
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-09-18 12:52:34 +0700

    Build ViewCart.js

--- frontend/src/components/cart/ProductInCart.js
+++ frontend/src/components/cart/ProductInCart.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import trashIcon from '../../assets/image/trash.png'
+import { useNavigate } from 'react-router-dom'
 
-const ProductInCart = ({item}) => {
+const ProductInCart = ({ item }) => {
+  const [productAmount, setProductAmount] = useState(item.quantity)
+  const navigate = useNavigate()
+
+  const increment = () => {
+    setProductAmount(prevAmount => prevAmount + 1)
+  }
+
+  const decrement = () => {
+    setProductAmount((prevQuantity) => (prevQuantity > 0 ? prevQuantity - 1 : 0));
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    // Allow only numbers
+    if (/^\d*$/.test(value)) {
+      // If value is empty, default to 1
+      if (value === '') {
+        setProductAmount(0);
+      } else {
+        const numericValue = parseInt(value, 10);
+        setProductAmount(numericValue);
+      }
+    }
+  };
+
+  const handleItemTrash = () => {
+    const localStorageCart = JSON.parse(localStorage.getItem('cart')) || []
+
+    const updatedCart = localStorageCart.filter(i => i.product_id !== item.product_id)
+
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+
+    console.log('Item removed. Updated cart:', updatedCart);
+
+    navigate('/cart')
+  }
+
   return (
-    <div>
+    <div className='w-full h-25 flex py-5 gap-5'>
+      <img src={item.product_image} alt={item.product_name} className='w-24 h-24 place-items-center object-cover rounded-md' />
 
+      <div className='w-2/5 flex flex-col px-12'>
+        <h2 className='text-2xl poppins-light'>{item.product_name}</h2>
+        <h3 className='text-2xl roboto-slab-regular'>${item.product_price}</h3>
+      </div>
+
+      <div className='w-1/4 h-14 border-y border-y-softBlack flex items-center justify-between poppins-regular'>
+        {/* TODO: BUTTON INCREMENT AND DECREMENT */}
+        <button
+          className='text-lg w-1/5 h-full'
+          onClick={decrement}
+        >
+          -
+        </button>
+        <input
+          placeholder=''
+          onChange={handleInputChange}
+          value={productAmount}
+          className='text-center w-max h-full bg-inherit'
+        />
+        <button
+          className='text-lg w-1/5 h-full'
+          onClick={increment}
+        >
+          +
+        </button>
+      </div>
+      <button
+        onClick={handleItemTrash}
+        className='w-8 h-8'
+      >
+        <img src={trashIcon} alt='trash' className='w-full h-full'/>
+      </button>
+      <p className='w-1/4 text-end text-2xl'>
+        $
+        {(item.product_price * item.quantity)}
+      </p>
     </div>
   )
 }
--- frontend/src/pages/products/ViewCart.js
+++ frontend/src/pages/products/ViewCart.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
 import ProductInCart from '../../components/cart/ProductInCart'
 import { Link } from 'react-router-dom'
 
 const ViewCart = () => {
+  const [productsInCart, setProductsInCart] = useState([])
+
+  // TODO: GET CART ITEM FROM LOCAL STORAGE
+  useEffect(() => {
+    const getProductsFromLocalStorage = () => {
+      const cartLocalStorage = JSON.parse(localStorage.getItem('cart'))
+      console.log(cartLocalStorage)
+
+      if (cartLocalStorage){
+        setProductsInCart(cartLocalStorage)
+      }
+    }
+    getProductsFromLocalStorage()
+  }, [])
+
   return (
-    <div className='w-auto h-screen bg-softWhite'>
+    <div className='w-auto h-auto bg-softWhite'>
       <header className='sticky top-0 h-auto w-full'>
         <Navbar />
       </header>
-      <main className='w-full px-20 py-32 flex flex-col'>
+      <main className='w-full h-full bg-inherit px-20 py-24 flex flex-col'>
 
         <div className='poppins-regular flex items-center justify-between'>
           <h1 className='p-5'>
@@ -23,11 +38,11 @@ const ViewCart = () => {
           </h2>
         </div>
 
-        <div className='text-lg flex items-center justify-between '>
+        <div className='text-lg flex items-center justify-between py-5'>
           <p className='p-5'>PRODUCT</p>
-          <div className='flex items-center'>
-            <p className='w-64 p-5'>QUANTITY</p>
-            <p className='w-64 text-end p-5'>TOTAL</p>
+          <div className='flex items-center justify-end px-5 gap-2'>
+            <p className='w-[18rem] py-5'>QUANTITY</p>
+            <p className='w-[18rem] text-end py-5'>TOTAL</p>
           </div>
         </div>
 
@@ -36,7 +51,9 @@ const ViewCart = () => {
         </div>
 
         <div className='p-5'>
-          <ProductInCart/>
+          {productsInCart.map(item => (
+            <ProductInCart item={item} key={item.product_name}/>
+          ))}
         </div>
 
         <div className='p-5'>
